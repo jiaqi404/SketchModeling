a1b3029774a4c9fd6187264076de26aeca25ce86
Jiaqi <marycheung021213@gmail.com>
2024-11-15 12:47:34 +0800

    Update Sketch-to-Image.py

--- src/Sketch-to-Image.py
+++ src/Sketch-to-Image.py
@@ -2,6 +2,11 @@ from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCM
 import torch
 from PIL import Image
 
+sketch_path = "src/tmp/sketch.png"
+img_path = "src/tmp/image.png"
+prompt = "bag" + "white background"
+negative_prompt = "black and white image"
+
 controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-scribble", torch_dtype=torch.float16, use_safetensors=True)
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
     "stable-diffusion-v1-5/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16, use_safetensors=True
@@ -9,11 +14,6 @@ pipe = StableDiffusionControlNetPipeline.from_pretrained(
 
 pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.enable_model_cpu_offload()
-
-sketch_path = "src/tmp/sketch.png"
-img_path = "src/tmp/image.png"
-prompt = "bag" + "white background"
-negative_prompt = "black and white image"
 
 canny_image=Image.open(sketch_path)
 output = pipe(prompt, negative_prompt=negative_prompt, image=canny_image).images[0]
